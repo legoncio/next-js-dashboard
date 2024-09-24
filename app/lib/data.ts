f263e64a979ed0d1000fa7ed773ad436eb25ae70
Leon Gonzalez <44852057+legoncio@users.noreply.github.com>
2024-09-24 12:34:30 -0500

    fix: remove artificial timeouts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,16 +11,7 @@ import { formatCurrency } from "./utils";
 
 export async function fetchRevenue() {
     try {
-        // Artificially delay a response for demo purposes.
-        // Don't do this in production :)
-
-        console.log("Fetching revenue data...");
-        await new Promise((resolve) => setTimeout(resolve, 3000));
-
         const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-        console.log("Data fetch completed after 3 seconds.");
-
         return data.rows;
     } catch (error) {
         console.error("Database Error:", error);
@@ -30,10 +21,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
     try {
-        // Artificially delay a response for demo purposes.
-        // Don't do this in production :)
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-
         const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -57,10 +44,6 @@ export async function fetchCardData() {
         // You can probably combine these into a single SQL query
         // However, we are intentionally splitting them to demonstrate
         // how to initialize multiple queries in parallel with JS.
-
-        // Artificially delay a response for demo purposes.
-        // Don't do this in production :)
-        await new Promise((resolve) => setTimeout(resolve, 1000));
 
         const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
         const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
